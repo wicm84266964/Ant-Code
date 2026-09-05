9cb27ff124704d3c04c259019383b6610bbb7204
wicm84266964 <lbj546865486@gmail.com>
2026-09-05 19:39:42 +0800

    release: prepare v2.0.7 dependency security patch

--- src/version.ts
+++ src/version.ts
@@ -2,7 +2,7 @@ import fs from "node:fs/promises";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-const FALLBACK_VERSION = "2.0.6";
+const FALLBACK_VERSION = "2.0.7";
 
 /**
  * @param {NodeJS.ProcessEnv} [env]
